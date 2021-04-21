f5a53be704efe61705be33e4e21f45c0360b675d
Renato Aloi <renato.aloi@gmail.com>
2021-04-20 23:57:20 -0300

    passing all tests

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
-def health_check_view():
-    pass
+def health_check_view(request):
+    return JsonResponse(data={})
