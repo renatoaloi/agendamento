412a0b1af5de76139ef39c627ebc70fafaceabf9
Renato Aloi <renato.aloi@gmail.com>
2021-04-20 22:48:23 -0300

    quotes pattern

--- api/functional_tests.py
+++ api/functional_tests.py
@@ -6,17 +6,17 @@ class AgendaFunctionalTest(unittest.TestCase):
 
     def setUp(self):
         self.my_name = self._get_test_name_from_a_very_long_string(self.__str__)
-        print(f"Test {self.my_name} up and running!")
+        print(f'Test {self.my_name} is up and running!')
 
     def tearDown(self):
-        print(f"Test {self.my_name} going down!")
+        print(f'Test {self.my_name} is going down!')
 
     def test_if_server_is_up_and_running(self):
         try:
             r = requests.get(os.environ.get('APP_HOST', 'http://localhost:8000/'))
             self.assertTrue(self.is_server_working(r.status_code))
         except Exception as e:
-            self.fail(f"Something went badly! Reason: {str(e)}")
+            self.fail(f'Something went badly! Reason: {str(e)}')
 
     def is_server_working(self, status_code):
         return status_code == 200
