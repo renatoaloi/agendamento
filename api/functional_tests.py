565a8ebc337cd99629a22845d46a4d80ec9565f1
Renato Aloi <renato.aloi@gmail.com>
2021-04-20 22:42:56 -0300

    f-string pattern

--- api/functional_tests.py
+++ api/functional_tests.py
@@ -6,17 +6,17 @@ class AgendaFunctionalTest(unittest.TestCase):
 
     def setUp(self):
         self.my_name = self._get_test_name_from_a_very_long_string(self.__str__)
-        print("Test %s up and running!" % self.my_name)
+        print(f"Test {self.my_name} up and running!")
 
     def tearDown(self):
-        print("Test %s going down!" % self.my_name)
+        print(f"Test {self.my_name} going down!")
 
     def test_if_server_is_up_and_running(self):
         try:
             r = requests.get(os.environ.get('APP_HOST', 'http://localhost:8000/'))
             self.assertTrue(self.is_server_working(r.status_code))
         except Exception as e:
-            self.fail("Something went badly! Reason: %s" % str(e))
+            self.fail(f"Something went badly! Reason: {str(e)}")
 
     def is_server_working(self, status_code):
         return status_code == 200
