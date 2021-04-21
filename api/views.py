cff2f5cd8b8f8a923006744b0b5c2c199419c3f4
Renato Aloi <renato.aloi@gmail.com>
2021-04-21 14:43:02 -0300

    futuramente verificar login

--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST, require_GET
 
+def futuramente_verificar_login(*params):
+    """ TODO: Verificar login pois o metodo atual retorna a lista toda, sem filtro de paciente """
+    pass
+
 @require_GET
 def health_check_view(request):
     return JsonResponse(data={})
 
 @require_GET
+@futuramente_verificar_login
 def agenda_list_view(request):
     return JsonResponse(data=[
         {
@@ -18,6 +23,7 @@ def agenda_list_view(request):
     ], safe=False)
 
 @require_POST
+@futuramente_verificar_login
 def agenda_create_view(request):
     return JsonResponse(data={
         'id': 0,
