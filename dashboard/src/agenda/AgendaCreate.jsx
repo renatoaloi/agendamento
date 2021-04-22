b4d23aabe16594ed4a2d382834378421d42739de
Renato Aloi <renato.aloi@gmail.com>
2021-04-22 15:09:13 -0300

    form fields validators

--- dashboard/src/agenda/AgendaCreate.jsx
+++ dashboard/src/agenda/AgendaCreate.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import "./Agenda.css";
 
 export default function AgendaCreate() {
+  const [especialidade, setEspecialidade] = useState("");
+  const [profissional, setProfissional] = useState("");
+  const [data, setData] = useState("");
+  const [hora, setHora] = useState("");
+  const [sending, setSending] = useState(false);
+
+  const onContinuar = (e) => {
+    e.preventDefault();
+    setSending(!sending);
+    e.target.value = "Aguarde...";
+    //alert(e.target.value);
+  };
+
   return (
     <>
       <div className="App-content">
         <form>
           <label>
             Especialidade
-            <select>
+            <select
+              required
+              value={especialidade}
+              onChange={(e) => setEspecialidade(e.target.value)}
+            >
               <option value="" selected>
                 Selecione...
               </option>
@@ -17,7 +35,11 @@ export default function AgendaCreate() {
 
           <label>
             Profissional
-            <select>
+            <select
+              required
+              value={especialidade}
+              onChange={(e) => setProfissional(e.target.value)}
+            >
               <option value="" selected>
                 Selecione...
               </option>
@@ -27,15 +49,33 @@ export default function AgendaCreate() {
 
           <label>
             Selecione o dia
-            <input type="date" name="data" />
+            <input
+              type="date"
+              name="data"
+              required
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+            />
           </label>
 
           <label>
             Selecione o horário
-            <input type="time" name="hora" />
+            <input
+              type="time"
+              name="hora"
+              required
+              value={hora}
+              onChange={(e) => setHora(e.target.value)}
+            />
           </label>
 
-          <input type="submit" value="Continuar" className="Btn-Submit" />
+          <input
+            type="submit"
+            value={sending ? "Aguarde..." : "Continuar"}
+            className="Btn-Submit"
+            onClick={(e) => onContinuar(e)}
+            disabled={sending}
+          />
         </form>
       </div>
     </>
