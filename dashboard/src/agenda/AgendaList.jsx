47c9622517b11b57287fb5118dd1ebee4cf37d4f
Renato Aloi <renato.aloi@gmail.com>
2021-04-22 20:27:46 -0300

    cors and listing from react

--- dashboard/src/agenda/AgendaList.jsx
+++ dashboard/src/agenda/AgendaList.jsx
@@ -1,35 +1,61 @@
+import { useState } from 'react';
+import axios from "axios";
+import moment from 'moment';
+
+function useConstructor(callBack = () => {}) {
+  const [hasBeenCalled, setHasBeenCalled] = useState(false);
+  if (hasBeenCalled) return;
+  callBack();
+  setHasBeenCalled(true);
+}
+
 export default function AgendaList() {
+  useConstructor(() => {
+    loadData();
+    console.log(
+      "This only happens ONCE and it happens BEFORE the initial render."
+    );
+  });
+
+  const [ data, setData ] = useState([]);
+
+  function loadData() {
+    axios
+      .get("/agenda/list")
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function findMonthByNumber(number) {
+    let a = [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov" ];
+    return a[number-1];
+  }
+
   return (
     <>
       <div className="App-content">
-        <div className="Card-List">
-          <div className="Card-List-Date">
-            <div>20</div>
-            <div>Abr</div>
+      {data.map((m) => { 
+        return(
+          <div className="Card-List">
+            <div className="Card-List-Date">
+              <div>{m.data.substr(0,2)}</div>
+              <div>{findMonthByNumber(m.data.substr(3, 2))}</div>
+            </div>
+            <div className="Card-List-Body">
+              <p className="Card-Titulo">
+                Chamada de vídeo com <br />
+                Dr. {m.profissional}
+              </p>
+              <p className="Card-Especialidade">{m.especialidade} - CRM {m.crm}</p>
+              <p className="Card-Data-Hora">Início as {m.hora}</p>
+            </div>
           </div>
-          <div className="Card-List-Body">
-            <p className="Card-Titulo">
-              Chamada de vídeo com <br />
-              Dr. Fulano
-            </p>
-            <p className="Card-Especialidade">Psicologia - CRM 1234</p>
-            <p className="Card-Data-Hora">Início as 12:12</p>
-          </div>
-        </div>
-        <div className="Card-List">
-          <div className="Card-List-Date">
-            <div>22</div>
-            <div>Mai</div>
-          </div>
-          <div className="Card-List-Body-Alternative">
-            <p className="Card-Titulo">
-              Chamada de vídeo com <br />
-              Dr. Fulano
-            </p>
-            <p className="Card-Especialidade">Psicologia - CRM 1234</p>
-            <p className="Card-Data-Hora">Início as 12:12</p>
-          </div>
-        </div>
+        );
+      })}
       </div>
     </>
   );
